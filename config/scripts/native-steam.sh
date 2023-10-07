207b1f656803ea96e3bb20d43789f78238d9cdc1
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2023-10-07 13:10:40 +0200

    fix: force replace files for the steam install

--- config/scripts/native-steam.sh
+++ config/scripts/native-steam.sh
@@ -10,7 +10,7 @@ sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ycollet-audinux.repo
 
 # Install Steam and Lutris into their own OCI layer
 # Add bootstraplinux_ubuntu12_32.tar.xz used by gamescope-session (Thanks ChimeraOS! - https://chimeraos.org/)
-rpm-ostree install \
+rpm-ostree install --force-replacefiles \
     mesa-dri-drivers.i686 \
     mesa-vulkan-drivers.i686 \
     vulkan-loader.i686 \
