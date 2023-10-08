79fdf9f481c9f986b4338a48dd60f159a1023bb2
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2023-10-08 22:10:13 +0200

    chore: remove virtual keyboard from sddm

--- config/scripts/deckstation-setup.sh
+++ config/scripts/deckstation-setup.sh
@@ -3,3 +3,7 @@ set -oue pipefail
 
 # Remove the autologin to gamescope-session
 rm /usr/etc/sddm.conf.d/steamos.conf
+
+# Remove virtual keyboard on SDDM
+# as this image is for workstations and permanently docked steamdecks
+rm /usr/etc/sddm.conf.d/virtualkbd.conf
