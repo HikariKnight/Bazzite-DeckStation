b0edc5802ef87b204c7b93af86c0baa8977c42a5
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2023-10-08 17:55:10 +0200

    chore: add back all repos and packages, seems like my system only has issues with x11

--- config/scripts/native-steam-simple.sh
+++ config/scripts/native-steam-simple.sh
@@ -1,40 +1,39 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-#sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_kylegospo-bazzite.repo && \
-#sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_kylegospo-bazzite-multilib.repo && \
+sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_kylegospo-bazzite.repo && \
+sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_kylegospo-bazzite-multilib.repo && \
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_kylegospo-hl2linux-selinux.repo && \
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_kylegospo-obs-vkcapture.repo && \
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_kylegospo-wallpaper-engine-kde-plugin.repo && \
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ycollet-audinux.repo
 
 # Simply install steam, mangohud, gamescope and gamescope-session
-# rpm-ostree install \
-#     vulkan-loader.i686 \
-#     alsa-lib.i686 \
-#     fontconfig.i686 \
-#     gtk2.i686 \
-#     libICE.i686 \
-#     libnsl.i686 \
-#     libxcrypt-compat.i686 \
-#     libpng12.i686 \
-#     libXext.i686 \
-#     libXinerama.i686 \
-#     libXtst.i686 \
-#     libXScrnSaver.i686 \
-#     mesa-libGL.i686 \
-#     mesa-libEGL.i686 \
-#     NetworkManager-libnm.i686 \
-#     nss.i686 \
-#     pulseaudio-libs.i686 \
-#     libcurl.i686 \
-#     systemd-libs.i686 \
-#     libva.i686 \
-#     libvdpau.i686 \
-#     libdbusmenu-gtk3.i686 \
-#     libatomic.i686 \
-#     pipewire-alsa.i686 && \
-
+rpm-ostree install \
+    vulkan-loader.i686 \
+    alsa-lib.i686 \
+    fontconfig.i686 \
+    gtk2.i686 \
+    libICE.i686 \
+    libnsl.i686 \
+    libxcrypt-compat.i686 \
+    libpng12.i686 \
+    libXext.i686 \
+    libXinerama.i686 \
+    libXtst.i686 \
+    libXScrnSaver.i686 \
+    mesa-libGL.i686 \
+    mesa-libEGL.i686 \
+    NetworkManager-libnm.i686 \
+    nss.i686 \
+    pulseaudio-libs.i686 \
+    libcurl.i686 \
+    systemd-libs.i686 \
+    libva.i686 \
+    libvdpau.i686 \
+    libdbusmenu-gtk3.i686 \
+    libatomic.i686 \
+    pipewire-alsa.i686 && \
 sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/rpmfusion-nonfree-steam.repo && \
 sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/rpmfusion-nonfree.repo && \
 sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/rpmfusion-nonfree-updates.repo && \
