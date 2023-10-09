735a5c8e15842dfc117d5d30b522a0be09272650
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2023-10-10 01:19:44 +0200

    feat: add deckstation just file

--- config/scripts/deckstation-setup.sh
+++ config/scripts/deckstation-setup.sh
@@ -8,5 +8,8 @@ rm /usr/etc/sddm.conf.d/steamos.conf
 # as this image is for workstations and permanently docked steamdecks
 rm /usr/etc/sddm.conf.d/virtualkbd.conf
 
+# Add deckstation justfile to 60-custom.just
+echo '!include /usr/share/ublue-os/just/65-deckstation.just' >> /usr/share/ublue-os/just/60-custom.just
+
 # Create SELinux context so libvirt can use looking-glass shm
 semanage fcontext -a -t svirt_tmpfs_t /dev/shm/looking-glass
