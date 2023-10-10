a1b30262a3cc8c8663efeb196e7077cf610e101f
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2023-10-10 02:04:02 +0200

    fix: make the hugepages script only need numbers from the config

--- config/files/usr/lib/systemd/hugetlb-reserve-pages.sh
+++ config/files/usr/lib/systemd/hugetlb-reserve-pages.sh
@@ -5,14 +5,17 @@ source /etc/default/hugepages_numa
 # PAGES = how many 1GB pages to reserve
 
 nodes_path=/sys/devices/system/node/
-if [ ! -d $nodes_path ]; then
-	echo "ERROR: $nodes_path does not exist"
+if [ ! -d ${nodes_path} ]; then
+	echo "ERROR: ${nodes_path} does not exist"
 	exit 1
 fi
 
 reserve_pages()
 {
-	echo "$1" > "$nodes_path/$2/hugepages/hugepages-1048576kB/nr_hugepages"
+	echo "${1}" > "${nodes_path}/node${2}/hugepages/hugepages-1048576kB/nr_hugepages"
+	mkdir /dev/hugepages1G
+	mount -t hugetlbfs -o pagesize=1G hugetblfs /dev/hugepages1G
 }
 
-reserve_pages "$PAGES" "$NODE"
+reserve_pages "${PAGES}" "${NODE}"
+
